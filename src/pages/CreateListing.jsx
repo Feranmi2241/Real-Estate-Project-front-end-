9b429e666ca1ce595baab5636d40a0034411113f
Oyeniyi Feranmi <feranmiayodeji24@gmail.com>
2026-02-08 08:57:56 -0800

    CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -324,7 +324,7 @@ const CreateListing = () => {
                 type="number"
                 id="regularPrice"
                 min="50"
-                max="10000000"
+                max="10000000000000"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
                 onChange={handleChange}
@@ -340,7 +340,7 @@ const CreateListing = () => {
                   type="number"
                   id="discountPrice"
                   min="0"
-                  max="10000000"
+                  max="10000000000"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
                   onChange={handleChange}
